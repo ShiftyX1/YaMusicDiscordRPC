33725a4ae87beb52ddf3b008314e78241f7dfa27
ShiftyX1 <ilia070403@gmail.com>
2023-10-17 01:53:34 +0300

    customtkinter update

--- generator.py
+++ generator.py
@@ -1,14 +1,13 @@
-from tkinter import Tk, ttk
-import tkinter as tk
+import customtkinter
 import sys
 import json
 
-class My_App(Tk):
+class My_App(customtkinter.CTk):
 
     def __init__(self):
-        Tk.__init__(self)
+        customtkinter.CTk.__init__(self)
         self.title('Генератор конфига для YaMusicRPC by ShiftyX1')
-        self.geometry("1200x400")
+        self.geometry("1000x200")
         self.resizable(False, False)
         self.setUI()
 
@@ -16,34 +15,37 @@ class My_App(Tk):
     def setUI(self):
         
         opts = { 'ipadx': 5, 'ipady': 5, 'sticky': 'nswe'  }
-        exit_button = ttk.Button(self, text='Выход', command=self.app_exit)
+        exit_button = customtkinter.CTkButton(self, text='Выход', command=self.app_exit)
         exit_button.grid(row=10, column=0, **opts)
 
-        self.bar2 = ttk.LabelFrame(self, text='Настройки')
+        self.bar2 = customtkinter.CTkFrame(self)
         self.bar2.grid(row=1, column=0, **opts)
 
-        self.entry_YaMusicAuth_label = ttk.Label(self.bar2, text='Токен Яндекс Музыки ')
+        self.label_settings = customtkinter.CTkLabel(self.bar2, text='Настройки')
+        self.label_settings.grid(row=0, column=0, **opts)
+
+        self.entry_YaMusicAuth_label = customtkinter.CTkLabel(self.bar2, text='Токен Яндекс Музыки ')
         self.entry_YaMusicAuth_label.grid(row=1, column=0, **opts)
 
-        self.entry_YaMusicAuth = ttk.Entry(self.bar2)
+        self.entry_YaMusicAuth = customtkinter.CTkEntry(self.bar2)
         self.entry_YaMusicAuth.grid(row=1, column=1, **opts)
 
-        self.entry_update_delay_label = ttk.Label(self.bar2, text='Задержка обновления RPC: ')
+        self.entry_update_delay_label = customtkinter.CTkLabel(self.bar2, text='Задержка обновления RPC: ')
         self.entry_update_delay_label.grid(row=2, column=2, **opts)
 
-        self.entry_update_delay = ttk.Entry(self.bar2)
+        self.entry_update_delay = customtkinter.CTkEntry(self.bar2)
         self.entry_update_delay.grid(row=1, column=4, **opts)
 
-        self.entry_start_delay_label = ttk.Label(self.bar2, text='Задержка старта работы RPC: ')
+        self.entry_start_delay_label = customtkinter.CTkLabel(self.bar2, text='Задержка старта работы RPC: ')
         self.entry_start_delay_label.grid(row=1, column=5, **opts)
 
-        self.entry_start_delay = ttk.Entry(self.bar2)
+        self.entry_start_delay = customtkinter.CTkEntry(self.bar2)
         self.entry_start_delay.grid(row=1, column=6, **opts)
 
-        self.button_generate = ttk.Button(self.bar2, text='Сгенерировать конфиг', command=self.generate_cfg)
+        self.button_generate = customtkinter.CTkButton(self.bar2, text='Сгенерировать конфиг', command=self.generate_cfg)
         self.button_generate.grid(row=8, column=0, **opts)
 
-        self.label_zatychka = ttk.Label(self.bar2, text='ГОТОВО :) Можете запускать RPC', foreground='#f5f5f5')
+        self.label_zatychka = customtkinter.CTkLabel(self.bar2, text='ГОТОВО :) Можете запускать RPC', fg_color='#f5f5f5')
         self.label_zatychka.grid(row=8, column=1, **opts)
 
     def app_exit(self):
